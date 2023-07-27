19009ff2a7d7e8312b809c0394f24330f09a933e
khush <khush.suthar@gmail.com>
2023-07-27 17:06:38 +0530

    dyanamic fetch

--- lib/getUserPosts.tsx
+++ lib/getUserPosts.tsx
@@ -1,7 +1,12 @@
 
 export default async function getUserPosts(userId:any) {
 
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    // As 2nd param can pass cache to determine the fetching static or dyanamic 
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+    {
+        cache:'no-store'
+    }  )
     if(!res) throw new Error("Failed to fetch")
     const data = await res.json()
     return data
