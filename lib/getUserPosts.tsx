9c7baba677bbcaf50e096e29743c66e79bd59f9a
khush <khush.suthar@gmail.com>
2023-07-27 17:10:46 +0530

    --ISR comment

--- lib/getUserPosts.tsx
+++ lib/getUserPosts.tsx
@@ -3,6 +3,7 @@ export default async function getUserPosts(userId:any) {
 
     // As 2nd param can pass cache to determine the fetching static or dyanamic
     // Or can use ISR and set the sec after which it will update  
+    // Or can export the revalidate in layout that will apply in all pages 
 
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
     {
