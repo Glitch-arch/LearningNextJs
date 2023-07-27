b32d0a9f3ee2872a10816a3f649912980f2f7d10
khush <khush.suthar@gmail.com>
2023-07-27 17:08:52 +0530

    -- ISR

--- lib/getUserPosts.tsx
+++ lib/getUserPosts.tsx
@@ -1,11 +1,12 @@
 
 export default async function getUserPosts(userId:any) {
 
-    // As 2nd param can pass cache to determine the fetching static or dyanamic 
+    // As 2nd param can pass cache to determine the fetching static or dyanamic
+    // Or can use ISR and set the sec after which it will update  
 
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
     {
-        cache:'no-store'
+        next: {revalidate:60}
     }  )
     if(!res) throw new Error("Failed to fetch")
     const data = await res.json()
